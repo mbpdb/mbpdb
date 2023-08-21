14e05e59d482604f9b4c2e0f0687d5857fe26dc7
Kuhfeldrf <kuhfeldrf@gmail.com>
2023-08-21 13:11:52 -0700

    added feature where TSV_Upload of new peptides can be accessed through admn/submmision/add which redirects to peptide_db_csv.html

--- include/peptide/peptide/admin.py
+++ include/peptide/peptide/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import PeptideInfo, Submission, Counter, ProteinInfo, ProteinVariant, protein_pid
 from .toolbox import pepdb_approve
+from django.urls import reverse
+from django.utils.html import format_html
 
 class PeptideInfoAdmin(admin.ModelAdmin):
     list_display = ('peptide','category')
+
 admin.site.register(PeptideInfo, PeptideInfoAdmin)
 
 class ProteinVariantAdmin(admin.ModelAdmin):
@@ -25,8 +28,26 @@ class SubmissionAdmin(admin.ModelAdmin):
 
     actions = [approve_submission]
 
+    #Added code to link to TSV_upload through the admin page. Added 8/21/23 RK
+    readonly_fields = ('add_csv_link',)
+    def add_csv_link(self, obj=None):
+        return format_html('<a href="{}">MBPDB add multiple entries using TSV file</a>', reverse('peptide_db_csv'))
+
+    add_csv_link.short_description = 'Upload TSV'
 admin.site.register(Submission, SubmissionAdmin)
 
 class CounterAdmin(admin.ModelAdmin):
     list_display = ('ip','access_time', 'page')
+
 admin.site.register(Counter, CounterAdmin)
+
+"""    list_display = ('peptide', 'time_approved')
+    add_form_template =  'peptide/peptide_db_csv.html'
+
+    def save_model(self, request, obj, form, change):
+        file = request.FILES.get('csv_file', False)
+        print(file)
+        if file:
+            handle_uploaded_file(file)
+        super().save_model(request, obj, form, change)
+"""
