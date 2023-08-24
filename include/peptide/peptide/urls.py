b7511177e27322bd37d2e86d6f5cbff07e685f63
Kuhfeldrf <kuhfeldrf@gmail.com>
2023-08-24 12:04:31 -0700

    removed unsed links and spec_list, pro_list, func_list

--- include/peptide/peptide/urls.py
+++ include/peptide/peptide/urls.py
@@ -4,20 +4,12 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.peptide_search, name='index'),
-    #re_path(r'^homology_search/$', views.homology_search, name='homology_search'),
     re_path(r'^pepex/$', views.pepex_tool, name='pepex'),
     re_path(r'^add_proteins/$', views.add_proteins_tool, name='add_proteins'),
     re_path(r'^protein_headers/$', views.protein_headers, name='protein_headers'),
     re_path(r'^tsv_search_results/', views.tsv_search_results, name='tsv_search_results'),
-    #re_path(r'^peptide_db/$', views.peptide_db, name='peptide_db'),
     re_path(r'^peptide_db_csv/$', views.peptide_db_csv, name='peptide_db_csv'),
     re_path(r'^peptide_search/$', views.peptide_search, name='peptide_search'),
-    re_path(r'^spec_list/$', views.spec_list, name='spec_list'),
-    re_path(r'^pro_list/$', views.pro_list, name='pro_list'),
-    re_path(r'^func_list/$', views.func_list, name='func_list'),
-
-    #re_path(r'^peptide', v_multi_search/$', views.peptide_multi_search, name='peptide_multi_search'),
-    #re_path(r'^contact/$', views.contact, name='contact'),
     re_path(r'^about_us/$', views.about_us, name='about_us'),
     re_path(r'^admin/', admin.site.urls),
 ]
