d42a07b970bbf93de79cbe9b415095461d9fe340
Rusty Kuhfeld <kuhfeldr@oregonstate.edu>
2025-08-19 11:17:00 -0700

    Remove virtual environment and cache files from tracking

--- include/peptide/peptide/notebooks/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Utility modules for peptide notebooks.
-"""
-
-from .uniprot_client import UniProtClient
-
-__all__ = ['UniProtClient'] 
--- include/peptide/peptide/observers.py
+++
@@ -1,5 +0,0 @@
-# observers.py
-
-class ProgressObserver:
-    def __init__(self, task_id):
-        self.task_id = task_id
